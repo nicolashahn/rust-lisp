5cb4bf982428763b2fc09a153596d642cf678077
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-07-09 22:58:38 -0400

    Format file with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -8,48 +8,38 @@ extern crate nom;
 
 //#[derive(Debug,PartialEq)]
 pub struct NomAstT {
-  pub tag:   String,
-  pub contents: String,
+    pub tag: String,
+    pub contents: String,
     //mpc_state_t state,
-  pub children_num: u8,
-  pub children: Vec<NomAstT>,
+    pub children_num: u8,
+    pub children: Vec<NomAstT>,
 }
 
 named!(token<&str, Vec<&str>>,
        ws!(many0!(alt!(tag_s!("b") | tag_s!("a"))))
-    );
+);
 
 named!(s_expr<&str,Vec<&str>>,
        delimited!(tag_s!("("),
                       token,
-                      tag_s!(")")
-       )
-
-
-
-    // do_parse!(
-    //   char!('(') >>
-    //   inner: many0!(ws!(alt!(tag_s!("b") | tag_s!("a")))) >>
-    //   char!(')') >>
-    //   (inner)
-    // )
+                  tag_s!(")"))
 );
 
-fn main () {
-  println!("Lispy Version 0.0.0.0.1");
-  println!("Press Ctrl+c to Exit\n");
+fn main() {
+    println!("Lispy Version 0.0.0.0.1");
+    println!("Press Ctrl+c to Exit\n");
 
-  loop {
-    print!("lispy> ");
-    let mut input = String::new();
-    io::stdout().flush().unwrap();
-    match io::stdin().read_line(&mut input) {
-      Ok(n) => {
-        println!("{} bytes read", n);
-        let res = s_expr(&input);
-        println!("response {:?}", res);
-      }
-      Err(error) => println!("error: {}", error),
+    loop {
+        print!("lispy> ");
+        let mut input = String::new();
+        io::stdout().flush().unwrap();
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                println!("{} bytes read", n);
+                let res = s_expr(&input);
+                println!("response {:?}", res);
+            }
+            Err(error) => println!("error: {}", error),
+        }
     }
-  }
 }
